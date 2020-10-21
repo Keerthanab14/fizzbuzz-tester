6c47cc21e1884179df3db675096e1f7005a67a20
Keerthana B <60786451+Keerthanab14@users.noreply.github.com>
2020-10-21 18:10:43 +0530

    Create fizzBuzz_test.java

---
+++ fizzBuzz_test.java
@@ -0,0 +1,14 @@
+package pl.pragmatists.pair;
+
+public class fizzBuzz {
+
+  String fizz = "Fizz";
+  String buzz = "Buzz";
+  String fizzbuzz = "FizzBuzz";
+
+  int fizzNumber = 3;
+  int buzzNumber = 5;
+  int fizzBuzzNumber = 15;
+
+  public FizzBuzz() {
+  }
