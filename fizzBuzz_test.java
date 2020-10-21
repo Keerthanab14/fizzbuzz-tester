7eac7ed45b80dd2ea90a525962f5c3fc256997bd
Keerthana B <60786451+Keerthanab14@users.noreply.github.com>
2020-10-21 18:29:23 +0530

    Update fizzBuzz_test.java

--- fizzBuzz_test.java
+++ fizzBuzz_test.java
@@ -2,13 +2,13 @@ package pl.pragmatists.pair;
 
 public class fizzBuzz {
 
-  String fizz = "Fizz";
-  String buzz = "Buzz";
-  String fizzbuzz = "FizzBuzz";
+  public String fizz = "Fizz";
+  public String buzz = "Buzz";
+  public String fizzbuzz = "FizzBuzz";
 
-  int fizzNumber = 3;
-  int buzzNumber = 5;
-  int fizzBuzzNumber = 15;
+  public int fizzNumber = 3;
+  public int buzzNumber = 5;
+  public int fizzBuzzNumber = 15;
 
   public fizzBuzz() {
   }
@@ -37,5 +37,24 @@ public class fizzBuzz {
     }
     return String.valueOf(i);
   }
+  
+   private boolean isFizzBuzzNumber(int num)
+   {
+    return (num % fizzBuzzNumber == 0)?1:0;
+   }
+  
+   private boolean isBuzzNumber(int num) 
+   {
+    return  (num % buzzNumber == 0)?1:0;
+  }
+
+  private boolean isFizzNumber(int num) 
+   {
+    return  (num % fizzNumber == 0)?1:0;
+  }
+
+ 
+  }
+
 
 
