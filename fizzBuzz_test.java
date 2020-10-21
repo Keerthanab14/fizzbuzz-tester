e41cedfc40f1d83b5d95755ae92dccb6a1bcf551
Keerthana B <60786451+Keerthanab14@users.noreply.github.com>
2020-10-21 19:01:54 +0530

    Update fizzBuzz_test.java

--- fizzBuzz_test.java
+++ fizzBuzz_test.java
@@ -2,6 +2,7 @@ package pl.pragmatists.pair;
 
 public class fizzBuzz {
 
+  //define constant values
   public String fizz = "Fizz";
   public String buzz = "Buzz";
   public String fizzbuzz = "FizzBuzz";
@@ -19,7 +20,7 @@ public class fizzBuzz {
     this.buzzNumber = buzzNumber;
     this.fizzBuzzNumber = fizzBuzzNumber;
   }
-  
+  //evaluates the number and returns the output to the string to be appended
   public String get_value(int num) {
     if(isFizzBuzzNumber(num)) 
     {
@@ -35,9 +36,10 @@ public class fizzBuzz {
     {
       return fizz;
     }
-    return String.valueOf(i);
+    return String.valueOf(num);
   }
   
+  //check if the number is either fizz,buzz or fizzbuzz
    private boolean isFizzBuzzNumber(int num)
    {
     return (num % fizzBuzzNumber == 0)?1:0;
@@ -53,12 +55,24 @@ public class fizzBuzz {
     return  (num % fizzNumber == 0)?1:0;
   }
 
- 
   }
 
+  //function to provide the maximum limit of the number and append the result to a modifiable string
+  public static String game(int upper_bound) {
+    StringBuilder s = new StringBuilder();
+
+    for (int i = 1; i <= upper_bound; i++) {
+      s.append(get_value(i)).append('\n');
+    }
+
+    return s.toString();
+  }
+  
+ //main method
   public static void main(String[] args)
   {
-    fizzBuzz fizzbuzzobj = new fizzbuzz();
+    fizzBuzz fizzbuzzobj = new fizzBuzz();
+    System.out.println(fizzbuzz.game(100));
   }
 }
 
